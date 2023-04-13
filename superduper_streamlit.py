f2e295e70ce0c3ce3a85b3fee1b79b0617fc2667
Nadya Agrawal <nadyaagrawal@gmail.com>
2023-04-13 12:25:59 -0400

    updating

---
+++ superduper_streamlit.py
@@ -0,0 +1,61 @@
+# simple_streamlit_app.py
+"""
+A streamlit app to provide dupe skincare products - 
+i.e. users choose an expensive product and the SuperDuper
+offers another that is both similar and cheaper.
+
+"""
+
+import streamlit as st
+from streamlit import session_state as session
+from notebook_4.ipynb import SuperDuper
+
+
+dataframe = None
+
+@st.cache(persist=True, show_spinner=False, suppress_st_warning=True)
+def load_data():
+    """
+    load and cache data
+    :return: products data
+    """
+    features = pd.read_csv("data/features.csv", index_col=0)
+    return features
+
+
+features = load_data()
+
+with open("data/products_data.pickle", "rb") as f:
+    products = pickle.load(f)
+
+
+st.title('SuperDuper')
+st.text('Dupe your favorite skincare product!')
+
+st.text("")
+st.text("")
+st.text("")
+st.text("")
+
+session.options = st.multiselect(label="Choose a brand from the dropdown menu", options=products["brand"])
+
+st.text("")
+st.text("")
+
+session.options = st.multiselect(label="Now choose a product", options=products["product_names"])
+
+st.text("")
+st.text("")
+
+is_clicked = col1.button(label="Recommend")
+
+if is_clicked:
+    dataframe = SuperDuper(session.options, product_name=products["product_names"])
+
+st.text("")
+st.text("")
+st.text("")
+st.text("")
+
+if dataframe is not None:
+    st.table(dataframe)
