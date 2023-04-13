cdf76dbab7d13457d8268a94e36ab43f4b9da9a8
Nadya Agrawal <nadyaagrawal@gmail.com>
2023-04-13 13:28:29 -0400

    updating

--- superduper_streamlit.py
+++ superduper_streamlit.py
@@ -6,21 +6,27 @@ offers another that is both similar and cheaper.
 
 """
 
-import streamlit as st
 import pandas as pd
+import streamlit as st
+import pickle
 
-products_data = pd.read_csv("data/products_data.csv")
-features = pd.read_csv("data/features.csv")
+# Load the data
+df_ing = pd.read_csv("data/products_data.csv")
 
+# Load the pickle file containing the features
+with open("features.pkl", "rb") as f:
+    features = pickle.load(f)
+
+# Fit the KNN model
 model = NearestNeighbors(n_neighbors=2, algorithm='ball_tree')
 model.fit(features)
-dist, idlist = model.kneighbors(features)
-
+dist, idlist = model.kneighbors(features)    
+    
 def SuperDuper(product_name):
     product_list = []
-    product_id = df_ing[df_ing["product_names"] == product_name].index
+    product_id = products[products["product_names"] == product_name].index
     product_id = product_id[0]
-    product_price = df_ing["price"].iloc[product_id] 
+    product_price = products["price"].iloc[product_id] 
     for newid in idlist[product_id]:
         name = df_ing.loc[newid].product_names
         brand = df_ing.loc[newid].brand
@@ -32,11 +38,19 @@ def SuperDuper(product_name):
         
     return product_list 
 
-brand_list = products_data["brand"].values
-selected_brand = st.selectbox( "Choose a brand from the dropdown menu", brand_list )
-product_list = products_data["product_names"].values
-selected_product = st.selectbox( "Choose a brand from the dropdown menu", product_list )
+st.title('SuperDuper')
 
-if st.button('Dupe!'):
-      recommended_products = SuperDuper(selected_product)
-      recommended_products
+# Create the dropdown menus
+brands = df_ing.brand.unique()
+selected_brand = st.selectbox("Choose a brand from the dropdown menu", brands)
+brand_products = df_ing[df_ing.brand == selected_brand].product_names.unique()
+selected_product = st.selectbox("Now pick a product", brand_products)
+
+# Call the SuperDuper function with the selected product and display the results
+results = SuperDuper(selected_product)
+if len(results) == 0:
+    st.write("No similar products found.")
+else:
+    st.write("Similar products:")
+    for product in results:
+        st.write(product)
